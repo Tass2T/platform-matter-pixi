d7eda6c491a48ebf6ac2057d3847d90e67b3e28b
Tass2T <griveau.bruno@gmail.com>
2024-01-20 00:53:24 +0100

    fixed some stuff and added player

--- src/game/level/index.ts
+++ src/game/level/index.ts
@@ -1,28 +1,41 @@
 import * as MATTER from "matter-js";
 import config from "../../../gameConfig.js";
-import Player from "@/game/player/index";
+import Player from "../player";
 
 export default class Level {
   physicEngine: MATTER.Engine;
   physicRenderer: MATTER.Render;
   player: Player;
   constructor() {
-    // INIT PHYSICS
+    this.initPhysicEngine();
+
+    // create two boxes and a ground
+    const ground = MATTER.Bodies.rectangle(
+      config.WIDTH / 2,
+      config.HEIGHT - 30,
+      config.WIDTH,
+      60,
+      {
+        isStatic: true,
+      }
+    );
+
+    this.player = new Player();
+
+    MATTER.Composite.add(this.physicEngine.world, [ground, this.player.body]);
+
+    // add all of the bodies to the world
+  }
+
+  initPhysicEngine() {
     this.physicEngine = MATTER.Engine.create();
     this.physicRenderer = MATTER.Render.create({
       element: document.body,
       engine: this.physicEngine,
     });
+
     this.physicRenderer.canvas.height = config.HEIGHT;
     this.physicRenderer.canvas.width = config.WIDTH;
-
-    // create two boxes and a ground
-    const ground = MATTER.Bodies.rectangle(400, 610, 810, 60, {
-      isStatic: true,
-    });
-
-    // add all of the bodies to the world
-    MATTER.Composite.add(this.physicEngine.world, [ground]);
 
     // run the renderer
     MATTER.Render.run(this.physicRenderer);
--- src/game/player/index.ts
+++ src/game/player/index.ts
@@ -2,5 +2,7 @@ import * as MATTER from "matter-js";
 
 export default class Player {
   body: MATTER.Body;
-  constructor() {}
+  constructor() {
+    this.body = MATTER.Bodies.rectangle(10, 10, 10, 10);
+  }
 }
