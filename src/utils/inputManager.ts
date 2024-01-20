cf00ffb8ca18c42ef2586581e05f42265c114371
Tass2T <griveau.bruno@gmail.com>
2024-01-20 01:33:34 +0100

    created input manager

---
+++ src/utils/inputManager.ts
@@ -0,0 +1,18 @@
+export default class InputManager {
+  pressedInput: Array<String> = [];
+  constructor() {
+    window.addEventListener("keydown", (e) => {
+      if (e.repeat) return;
+
+      if (!this.pressedInput.includes(e.code)) this.pressedInput.push(e.code);
+    });
+
+    window.addEventListener("keyup", (e) => {
+      this.pressedInput = this.pressedInput.filter((code) => code !== e.code);
+    });
+  }
+
+  getPressedInputs() {
+    return this.pressedInput;
+  }
+}
