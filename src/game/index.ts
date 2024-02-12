6d6abc1c3752a822de0cbadc680030f39317de2c
tass2t <brunog90@gmail.com>
2024-02-12 16:53:00 +0100

    start configuring PIXI

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,13 +1,15 @@
 import Level from "./level";
 import * as PIXI from "pixi.js";
+import config from "../../gameConfig.js";
 
 export default class Game {
   pixiApp: PIXI.Application;
   level: Level;
   constructor() {
     this.pixiApp = new PIXI.Application({
-      height: 0,
-      width: 0,
+      height: config.HEIGHT,
+      width: config.WIDTH,
+      background: "#aabbff",
     });
     document.body.appendChild(this.pixiApp.view as HTMLCanvasElement);
     this.level = new Level();
