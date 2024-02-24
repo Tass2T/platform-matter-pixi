c509e9ccfb981b7f9e0bd97966bee060d99123c2
Tass2T <griveau.bruno@gmail.com>
2024-02-24 22:39:40 +0100

    added brand new jumping system

--- gameConfig.js
+++ gameConfig.js
@@ -1,11 +1,12 @@
 export default {
   HEIGHT: 700,
   WIDTH: 1100,
-  SPEED: 4,
+  SPEED: 6,
   player: {
     xAxisStart: 150,
     jumpNumber: 2,
-    jumpSpeed: 20,
+    baseJumpSpeed: 12,
+    velocityLoss: 0.3,
     mass: 2,
   },
   platForm: {
