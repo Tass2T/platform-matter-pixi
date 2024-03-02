6e0ed0a9a97f67046b288f150a19a15f9e2d6120
Tass2T <griveau.bruno@gmail.com>
2024-03-03 00:54:26 +0100

    simplified platform manager

--- src/game/platformManager/index.ts
+++ src/game/platformManager/index.ts
@@ -2,72 +2,48 @@ import * as MATTER from "matter-js";
 import * as PIXI from "pixi.js";
 import config from "../../../gameConfig.js";
 import Platform from "../platform/index.js";
-import Diamond from "../diamond/index.js";
 
 export default class PlatformManager {
   _engine: MATTER.Engine;
   _platFormList: Array<Platform> = [];
-  _diamondList: Array<Diamond> = [];
   _gameSpeed: number = config.SPEED;
   constructor(engineWorld: MATTER.Engine, parentContainer: PIXI.Container) {
     this._engine = engineWorld;
-    this.createInitialPlatForms(parentContainer);
-    this.createAdditionnalPlatforms(parentContainer);
-    this.registerPlatformAndDiamonds();
+    this.createPlatforms(parentContainer);
   }
 
   getGamespeed() {
     return this._gameSpeed;
   }
 
-  getAllObjects(): Array<Diamond | Platform> {
-    return [...this._platFormList, ...this._diamondList];
+  getPlatformList() {
+    return this._platFormList;
   }
 
   increaseGamespeed() {
     if (this._gameSpeed < config.MAXSPEED) this._gameSpeed += 0.05;
   }
 
-  createInitialPlatForms(levelContainer: PIXI.Container) {
-    const ground = new Platform(
-      "standard",
-      config.platForm.start,
-      levelContainer,
-      true
-    );
-    this._diamondList.push(...ground.getDiamondList());
-    this._platFormList.push(ground);
+  setGameSpeed(newValue: number): void {
+    this._gameSpeed = newValue;
   }
 
-  createAdditionnalPlatforms(levelContainer: PIXI.Container) {
-    for (let i = 1; i <= 5; i++) {
-      const platForm = new Platform(
-        "standard",
-        this._platFormList[this._platFormList.length - 1].getRightCoord() +
-          this.setAjustedGap(),
-        levelContainer
-      );
-      this._platFormList.push(platForm);
-      this._diamondList.push(...platForm.getDiamondList());
+  createPlatforms(levelContainer: PIXI.Container) {
+    for (let i = 1; i <= 6; i++) {
+      const x =
+        i === 1
+          ? config.platForm.start
+          : this._platFormList[this._platFormList.length - 1].getRightCoord() +
+            this.setAjustedGap();
+      const ground = new Platform("standard", x, levelContainer, i === 1);
+
+      this._platFormList.push(ground);
+      MATTER.Composite.add(this._engine.world, ground.getBody());
     }
-  }
-
-  registerPlatformAndDiamonds() {
-    this._platFormList.forEach((platform) => {
-      MATTER.Composite.add(this._engine.world, platform.getBody());
-
-      platform.getDiamondList().forEach((diamond) => {
-        MATTER.Composite.add(this._engine.world, diamond.getBody());
-      });
-    });
   }
 
   setAjustedGap(): number {
     return config.platForm.gap * (Math.random() * 3) + this._gameSpeed;
-  }
-
-  setGameSpeed(newValue: number): void {
-    this._gameSpeed = newValue;
   }
 
   getFarfestXCoord(): number {
@@ -91,14 +67,6 @@ export default class PlatformManager {
         platForm.moveLeft(this._gameSpeed * delta);
       }
     });
-  }
-
-  getPlatformList() {
-    return this._platFormList;
-  }
-
-  getDiamondList() {
-    return this._diamondList;
   }
 
   syncPlatforms(delta: number) {
