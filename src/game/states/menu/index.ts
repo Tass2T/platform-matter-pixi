c40dd3eb096d19735e22d629ee211a55237f27a6
Tass2T <griveau.bruno@gmail.com>
2024-03-31 19:21:14 +0200

    added first elements for main menu

--- src/game/states/menu/index.ts
+++ src/game/states/menu/index.ts
@@ -1,4 +1,4 @@
-import { Container, Graphics } from "pixi.js";
+import { AnimatedSprite, Assets, Container, Sprite } from "pixi.js";
 import config from "../../../../gameConfig.js";
 import ScoreBoard from "../../components/scoreBoard";
 import GameState from "../../traits/GameState";
@@ -14,12 +14,25 @@ export default class Menu extends GameState {
     this.initBackground();
   }
 
-  initBackground() {
-    const background = new Graphics()
-      .rect(0, 0, config.WIDTH, config.HEIGHT)
-      .fill("green");
+  async initBackground() {
+    const menubundle = await Assets.loadBundle("menu");
+    console.log(menubundle);
 
-    this._stateContainer.addChild(background);
+    const textureSprite = new Sprite(menubundle.background);
+    const persoSprite = new AnimatedSprite(
+      menubundle.persoBody.animations.closeEye
+    );
+    persoSprite.anchor.set(0.5, 0.5);
+    persoSprite.height = config.HEIGHT * 0.9;
+    persoSprite.width = persoSprite.height;
+
+    persoSprite.position.set(config.WIDTH / 2, config.HEIGHT / 2);
+
+    textureSprite.position.set(0, 0);
+    textureSprite.height = config.HEIGHT;
+    textureSprite.width = config.WIDTH;
+
+    this._stateContainer.addChild(textureSprite, persoSprite);
   }
 
   start() {
