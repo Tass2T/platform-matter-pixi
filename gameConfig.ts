c186557e3764b8504a756811a3d80b4f85eb6e0d
Tass2T <brunog90@gmail.com>
2024-04-07 23:11:28 +0200

    add type to game config

---
+++ gameConfig.ts
@@ -0,0 +1,61 @@
+interface configType {
+  HEIGHT: number,
+  WIDTH: number,
+  SPEED: number,
+  GRAVITY: number,
+  MAXSPEED: number,
+  COUNTDOWN: number,
+  PHYSIC_DEBUG_MODE: boolean,
+  player: {
+    xAxisStart: number,
+    baseJumpSpeed: number,
+    velocityLoss: number,
+  },
+  platForm: {
+    gap: number,
+    start: number,
+    standard: {
+      width: number,
+      height: number,
+    },
+  },
+  diamond: {
+    side: number,
+    height: number,
+    nb: number,
+    gap: number,
+    points: number,
+  },
+}
+
+const config: configType = {
+  HEIGHT: 700,
+  WIDTH: 1200,
+  SPEED: 6,
+  GRAVITY: 1.2,
+  MAXSPEED: 15,
+  COUNTDOWN: 3,
+  PHYSIC_DEBUG_MODE: false,
+  player: {
+    xAxisStart: 150,
+    baseJumpSpeed: 220,
+    velocityLoss: 11,
+  },
+  platForm: {
+    gap: 100,
+    start: 200,
+    standard: {
+      width: 300,
+      height: 40,
+    },
+  },
+  diamond: {
+    side: 35,
+    height: 35,
+    nb: 3,
+    gap: 40,
+    points: 500,
+  },
+};
+
+export default config
