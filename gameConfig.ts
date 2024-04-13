5794849f2c96b929712cdde35254ea997fe2965d
Tass2T <griveau.bruno@gmail.com>
2024-04-13 17:57:55 +0200

    game is fullscreen

--- gameConfig.ts
+++ gameConfig.ts
@@ -1,36 +1,36 @@
 interface configType {
-  HEIGHT: number,
-  WIDTH: number,
-  SPEED: number,
-  GRAVITY: number,
-  MAXSPEED: number,
-  COUNTDOWN: number,
-  PHYSIC_DEBUG_MODE: boolean,
+  HEIGHT: number
+  WIDTH: number
+  SPEED: number
+  GRAVITY: number
+  MAXSPEED: number
+  COUNTDOWN: number
+  PHYSIC_DEBUG_MODE: boolean
   player: {
-    xAxisStart: number,
-    baseJumpSpeed: number,
-    velocityLoss: number,
-  },
+    xAxisStart: number
+    baseJumpSpeed: number
+    velocityLoss: number
+  }
   platForm: {
-    gap: number,
-    start: number,
+    gap: number
+    start: number
     standard: {
-      width: number,
-      height: number,
-    },
-  },
+      width: number
+      height: number
+    }
+  }
   diamond: {
-    side: number,
-    height: number,
-    nb: number,
-    gap: number,
-    points: number,
-  },
+    side: number
+    height: number
+    nb: number
+    gap: number
+    points: number
+  }
 }
 
 const config: configType = {
-  HEIGHT: 700,
-  WIDTH: 1200,
+  HEIGHT: window.innerHeight,
+  WIDTH: window.innerWidth > 1920 ? 1920 : window.innerWidth,
   SPEED: 6,
   GRAVITY: 1.2,
   MAXSPEED: 15,
@@ -56,6 +56,6 @@ const config: configType = {
     gap: 40,
     points: 500,
   },
-};
+}
 
 export default config
