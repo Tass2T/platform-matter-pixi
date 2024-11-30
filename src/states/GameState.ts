26cb3b67d888476f32311afdbaed2b09226e2fb7
Tass2T <griveau.bruno@gmail.com>
2024-11-30 16:24:46 +0100

    gamestate gère plus la pause

--- src/states/GameState.ts
+++ src/states/GameState.ts
@@ -7,8 +7,6 @@ export default class GameState extends Container implements AppScreen {
   #game: Game
   #ui: GameUI
 
-  #isPausing = false
-
   constructor() {
     super()
   }
@@ -16,11 +14,15 @@ export default class GameState extends Container implements AppScreen {
   prepare = (): Promise<void> => {
     return new Promise(async (resolve): Promise<void> => {
       this.#game = new Game()
-      this.#game.zIndex = 1
       this.#ui = new GameUI()
+
+      this.#game.zIndex = 1
       this.#ui.zIndex = 4
+
       await this.#game.prepare()
       this.addChild(this.#game, this.#ui)
+
+      await this.#ui.startCountDown()
       resolve()
     })
   }
@@ -28,7 +30,6 @@ export default class GameState extends Container implements AppScreen {
   checkIfGameOver() {
     if (this.#game.getPlayer().getHasFallen()) {
       this.#game.pause()
-      this.#isPausing = true
       this.#ui.startGameOver()
     }
   }
@@ -37,6 +38,6 @@ export default class GameState extends Container implements AppScreen {
     this.#game.update()
     this.#ui.setScore(this.#game.getScore())
     this.#ui.update()
-    if (!this.#isPausing) this.checkIfGameOver()
+    this.checkIfGameOver()
   }
 }
