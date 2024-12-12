3cc2c1bf078bce529c1e8ddd6c6027fef98c9577
Tass2T <griveau.bruno@gmail.com>
2024-12-12 11:39:56 +0100

    added localStorageManager.ts

---
+++ src/utils/localStorageManager.ts
@@ -0,0 +1,31 @@
+import { StoredData } from '../models'
+
+class LocalStorageManager {
+  #storedData: StoredData = { bestScore: 0, soundIsMuted: false }
+  constructor() {
+    if (localStorage.getItem('storedData')) JSON.parse(localStorage.getItem('storedData') as string)
+  }
+
+  private saveDatas = () => {
+    localStorage.setItem('storedData', JSON.stringify(this.#storedData))
+  }
+
+  getBestScore = () => {
+    return this.#storedData.bestScore
+  }
+
+  getIsMuted = () => {
+    return this.#storedData.soundIsMuted
+  }
+
+  switchIsMuted = () => {
+    this.#storedData.soundIsMuted = !this.#storedData.soundIsMuted
+  }
+
+  setBestScore = (bestScore: number) => {
+    this.#storedData.bestScore = bestScore
+    this.saveDatas()
+  }
+}
+
+export const localStorageManager = new LocalStorageManager()
